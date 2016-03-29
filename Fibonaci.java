452c56445b8088c5fff3065b6ff85d01afac0973
Andrean Joko Julianto <andreanjoko26@gmail.com>
2016-03-29 15:33:30 +0700

    Added files via upload

---
+++ Fibonaci.java
@@ -0,0 +1,36 @@
+package jurnal1;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ 
+/**
+ *
+ * @author Praktikum
+ */
+public class Fibonaci {
+    
+    private ArrayList<Integer> al = new ArrayList<>();
+    
+    public Fibonaci() {
+    }
+    
+    public void performFibonaci(int i){
+        int prevInt = 0;
+        int nextInt = 1;
+        for (int j = 0; j < i; j++) {
+            if ( j == 0 || j == 1){
+                System.out.println("1");
+                al.add(1);
+                continue;
+            }
+            al.add(al.get(j-1)+al.get(j-2));
+            System.out.println("" + al.get(j));
+        }
+        
+    }
+    
+    
+}
---
+++ Jurnal1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal1;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Jurnal1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ReverseFibonaci fibonaci = new ReverseFibonaci();
+        fibonaci.performReverseFibonaci(4);
+    }
+    
+}
